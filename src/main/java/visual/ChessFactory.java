4eaa0334ac0a66294b592c81a9903ae780e27915
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2021-02-08 21:54:02 +0100

    Images finally found

--- src/main/java/visual/ChessFactory.java
+++ src/main/java/visual/ChessFactory.java
@@ -1,2 +1,33 @@
-package visual;public class ChessFactory {
+package visual;
+
+import framework.Game;
+import minidraw.framework.Drawing;
+import minidraw.framework.DrawingEditor;
+import minidraw.framework.DrawingView;
+import minidraw.framework.Factory;
+
+import javax.swing.*;
+
+public class ChessFactory implements Factory {
+
+    private final Game game;
+
+    public ChessFactory(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public DrawingView createDrawingView(DrawingEditor editor) {
+        return null;
+    }
+
+    @Override
+    public Drawing createDrawing(DrawingEditor editor) {
+        return null;
+    }
+
+    @Override
+    public JTextField createStatusField(DrawingEditor editor) {
+        return null;
+    }
 }
--- src/main/java/visual/ShowChess.java
+++ src/main/java/visual/ShowChess.java
@@ -1,7 +1,20 @@
 package visual;
 
+import framework.Game;
+import minidraw.framework.DrawingEditor;
+import minidraw.standard.MiniDrawApplication;
+import standard.StandardGame;
+
 public class ShowChess {
     public static void main(String[] args) {
-        System.out.println("showChess virker");
+        Game game = new StandardGame();
+
+        DrawingEditor editor =
+                new MiniDrawApplication( "Move any unit using the mouse",
+                        new ChessFactory(game));
+        editor.open();
+        editor.showStatus("Try to play SemiCiv");
+
+        //editor.setTool( new CompositionTool(editor, game));
     }
 }
